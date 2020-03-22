da6fe1c7bae4473afd5b94d4e2700914ee4a622a
MohammedBasha <me@mohammedbasha.com>
2020-03-22 14:44:14 +0200

    Explain is_writable() or is_writeable()

--- index.php
+++ index.php
@@ -2,17 +2,16 @@
 
 // dirname(__FILE__) => getting the directory path that holds the index.php file + then store a full pathe of the text file in $file.
 
-$file = dirname(__FILE__) . '\m!b.txt';
+$file = dirname(__FILE__) . '\mb.txt';
 
-// Checks if the $file exists then print a success message
+// Checks if the $file is writable then print a success message and write to it.
 
-if(file_exists($file)):
-	echo 'The file [' . basename($file) . '] exists';
+// if(is_writeable($file)):
+if(is_writable($file)):
+	echo 'The file [' . basename($file) . '] writable';
+	file_put_contents($file, 'Writable by PHP');
 else:
 
-	// If not exists print a fail message and create the file with content.
-
-	echo 'The file [' . basename($file) . '] not exists, but It\'s created by PHP';
-	file_put_contents($file, 'Created by PHP');
-	
+	// If It's not writable print a fail message.
+	echo 'The file [' . basename($file) . '] not writable';
 endif;
