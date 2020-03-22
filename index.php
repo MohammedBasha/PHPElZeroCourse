cb413d1eca589650aeb91efb5373c2961c5b5584
MohammedBasha <me@mohammedbasha.com>
2020-03-22 14:21:57 +0200

    Explain dirname(), __FILE__, basename(), file_exists() and file_put_contents()

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-	// how to use the Error control operator
-	$file = @fopen('file.txt', 'r') or die('This file is not found');
+
+// dirname(__FILE__) => getting the directory path that holds the index.php file + then store a full pathe of the text file in $file.
+
+$file = dirname(__FILE__) . '\m!b.txt';
+
+// Checks if the $file exists then print a success message
+
+if(file_exists($file)):
+	echo 'The file [' . basename($file) . '] exists';
+else:
+
+	// If not exists print a fail message and create the file with content.
+
+	echo 'The file [' . basename($file) . '] not exists, but It\'s created by PHP';
+	file_put_contents($file, 'Created by PHP');
+	
+endif;
