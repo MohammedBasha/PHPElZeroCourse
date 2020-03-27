bc60206db950027bedabfe9e7c100fa0fcff818d
MohammedBasha <me@mohammedbasha.com>
2020-03-27 23:05:39 +0200

    executing a query for adding a produt row in the items table

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
 	try { // Try to connect to the database
 		$db = new PDO($dsn, $user, $pass); // Connect to the Database with the PDO Class
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		echo 'You are Connected';
+		$q = "INSERT INTO items (name) VALUES ('product3')";
+		$db->exec($q);
 
 	} catch(PDOException $e) { // catch any errors while connecting
 		echo 'Connection Failed >>> ' . $e->getMessage();
