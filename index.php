e6ba8e5f6a4e710c57e8463cca904c9247252fe0
MohammedBasha <me@mohammedbasha.com>
2020-03-27 22:53:39 +0200

    Setting the attribute mode

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 
 	try { // Try to connect to the database
 		$db = new PDO($dsn, $user, $pass); // Connect to the Database with the PDO Class
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		echo 'You are Connected';
 
 	} catch(PDOException $e) { // catch any errors while connecting
