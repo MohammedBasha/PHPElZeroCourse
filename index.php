dee0bb2f581648c0645c4f49638d7e1d44cd03a6
MohammedBasha <me@mohammedbasha.com>
2020-03-27 23:12:58 +0200

    Adding a support option for arabic language

--- index.php
+++ index.php
@@ -3,12 +3,15 @@
 	$dsn = 'mysql:host=localhost;dbname=ecom2'; // Data Source Name
 	$user = 'root'; // Database Username
 	$pass = ''; // Database Password
+	$options = array(
+		PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+	);
 
 
 	try { // Try to connect to the database
-		$db = new PDO($dsn, $user, $pass); // Connect to the Database with the PDO Class
+		$db = new PDO($dsn, $user, $pass, $options); // Connect to the Database with the PDO Class
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$q = "INSERT INTO items (name) VALUES ('product3')";
+		$q = "INSERT INTO items (name) VALUES ('منتج 5')";
 		$db->exec($q);
 
 	} catch(PDOException $e) { // catch any errors while connecting
