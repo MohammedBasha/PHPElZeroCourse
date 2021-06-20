569f2a8829d316fc94eb80231c671e07b8834b2e
MohammedBasha <me@mohammedbasha.com>
2021-06-20 12:44:26 +0200

    Deal With Checkboxes Insert With PHP

--- index.php
+++ index.php
@@ -1,19 +1,43 @@
 <?php
 
-$dir = __DIR__;
+	// Checking if the requesting method used in the form is a post method
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-echo '<pre>' . $dir . '<br>';
+		// echo the username field
+		echo $_POST['username'] . '<br>';
 
-print_r(scandir($dir));
+		// collect the browsers array values in a variable
+		$browsers = implode(',', $_POST['browser']);
 
-echo '<br>';
+		// This is the data coming from the database
+		echo $browsers . '<br>';
 
-foreach(scandir($dir) as $file) {
-	if (pathinfo($file, PATHINFO_EXTENSION) === 'php') {
+		// splitting the data that came from database
+		$splitted = explode(',', $browsers);
 
-		// require or include could be used here
-		echo 'The php file name is ' . $file . ' and its full path is ' . $dir . '\\' . $file . '<br>';
+		foreach($splitted as $split) {
+			echo '<a href=' . $split . ' title=' . $split . '>' . $split . '</a><br>';
+		}
 	}
-}
+?>
 
-echo '</pre>';
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Deal With Checkboxes Insert With PHP</title>
+	</head>
+	<body>
+		<!-- Sending the Data to the same page with the action attribute value like below -->
+		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+			<input type="text" name="username" placeholder="User Name"><br>
+			<input type="checkbox" name="browser[]" value="chrome" id="chrome">
+			<label for="chrome">Google Chrome</label><br>
+			<input type="checkbox" name="browser[]" value="opera" id="opera">
+			<label for="opera">Opera</label><br>
+			<input type="checkbox" name="browser[]" value="firefox" id="firefox">
+			<label for="firefox">Firefox</label><br>
+			<input type="submit" value="Add" title="Add">
+		</form>
+	</body>
+</html>
