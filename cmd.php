c6b6c199992171bcc8109b82586661cd7b6bb1b3
MohammedBasha <me@mohammedbasha.com>
2021-06-23 12:14:27 +0200

    #04 - Connect With PDO + Examples

---
+++ cmd.php
@@ -0,0 +1,30 @@
+<?php
+
+// We can deal with the Database with the following ways:
+
+// 1- Command line.
+// 2- phpMyAdmin.
+// 3- MySQL Workbench App.
+
+// 1- With Command Line:
+
+// Open CMD and login with the user
+// > mysql -u root
+
+// To create a database
+// > CREATE DATABASE ecom;
+
+// To show databases:
+// > show databases;
+
+// to drop a database
+// > DROP DATABASE ecom;
+
+// to use database
+// > use ecom;
+
+// to show tables in database
+// > show tables;
+
+// to show all in database
+// > SELECT * FROM rowName;
--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <?php
+
+$dsn = 'mysql:host=localhost;dbname=ecom'; // Data source name
+$user = 'root'; // Database user
+$pass = ''; // Database password
+$options = [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'];
+
+try {
+    $db = new PDO ($dsn, $user, $pass, $options); // Start a new connection with PDO class
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'You\'re connected';
+} catch(PDOException $e) {
+    echo 'Failed connection due to ' . $e->getMessage();
+}
