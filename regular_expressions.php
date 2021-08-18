1d2e37b8e08f96ff9040b91968c046e4566c7d16
MohammedBasha <me@mohammedbasha.com>
2021-08-18 04:43:31 +0200

    Talking about Character Classes, Repetition Meta Characters, Quantified repetition, articles about Modes: Greedy and Lazy, Capturing Group, and Alternation in regular expressions

--- regular_expressions.php
+++ regular_expressions.php
@@ -1,9 +1,12 @@
 <?php
 
-/* - https://www.oreilly.com/library/view/mastering-regular-expressions/0596528124/
- * - https://www.php.net/manual/en/book.pcre.php
+/* - https://www.php.net/manual/en/book.pcre.php
+ * - https://www.oreilly.com/library/view/mastering-regular-expressions/0596528124/
  * - https://rexegg.com/regex-quantifiers.html
  * - https://www.regular-expressions.info/
+ * 
+ * For testing Regular expressions:
+ * - https://regex101.com/
  * 
  * PHP depend on two kinds of regular expressions:
  * 1- PCRE: Perl compatible regular expressions (built-in).
@@ -17,8 +20,35 @@
  * - Delimeters: the symbols that the pattern is put inside it like // or ##.
  * 
  * Character Sets:
+ * 
  * Characters Ranges:
+ * 
  * Negation Meta Character - Negative Character Sets
+ * 
+ * Character Classes:
+ * - \d : look for any digit like [0-9]
+ * - \w : look for any alphanumeric character or underscore _ like [a-zA-Z0-9_]
+ * - \s : look for any whitespace like [ \t\r\n]
+ * - \D : look for any non-digit character [^0-9]
+ * - \W : look for any non-alphanumeric character or underscore _ [^a-zA-Z0-9_]
+ * - \S : look for any non-whitespace like [^ \t\r\n]
+ * - \b : word boundary
+ * - \x : unicode characters
+ * 
+ * Repetition Meta Characters:
+ * Multiply Character * : the preceding item can be repeated zero or more times
+ * Plus Character + : the preceding item can be repeated one or more times
+ * Question Mark Character ? : the preceding item can be repeated Zero or one time
+ * 
+ * Quantified repetition: {min,max} or {min,} or {min}
+ * 
+ * Modes: Greedy and Lazy
+ * - https://www.regular-expressions.info/repeat.html
+ * - https://www.rexegg.com/regex-quantifiers.html
+ * 
+ * Capturing Group: searching for all the characters found between (abc) (\w+)
+ * 
+ * Alternation: searching for all the grouped characters found between those alternatives (abc|def)
  * 
  */
 
@@ -73,3 +103,15 @@
 //var_dump($match);
 //echo '</pre>';
 
+// Quantified repetition
+//$pattern = '/9{2}/';
+//$pattern = '/9{2,3}/';
+//$str = 'I have 9999 Dollars'; // as it's greedy, it will return 999 over 99
+
+//$pattern = '/\w+_\d{4}\.\w{3}/';
+//$pattern = '/\w+?_\d{2,4}\.[a-z]{3,5}/'; // when using the ? it will be lazy mode
+//$str = 'file07_2015.xls, doc04_2018.doc, document09_2020.pwt';
+//preg_match_all($pattern, $str, $match); // returns all the matchs
+//echo '<pre>';
+//var_dump($match);
+//echo '</pre>';
