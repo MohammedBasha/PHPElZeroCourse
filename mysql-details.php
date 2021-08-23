771fa4ab19fd1d1e6b655fdf4aa19e553b301417
MohammedBasha <me@mohammedbasha.com>
2021-08-23 08:25:33 +0200

    Working on some table and column commands

--- mysql-details.php
+++ mysql-details.php
@@ -26,6 +26,8 @@
  * - if you want to connect to another server rather than localhost use -h
  * > mysql -u root -h server.name -p
  * 
+ * DATABASE:
+ * 
  * - to show databases:
  * > show databases;
  * 
@@ -68,6 +70,8 @@
  * - to drop db:
  * > DROP DATABASE IF EXISTS company;
  * 
+ * TABLES:
+ * 
  * - https://dev.mysql.com/doc/refman/8.0/en/create-table.html
  * - create table, add column attributes and constraints:
  * - UNSIGNED: means no negative values.
@@ -84,11 +88,43 @@
  * - create table like another table with the same structure:
  * > CREATE TABLE IF NOT EXISTS table_name LIKE table2_name;
  * 
+ * - create table with the same structure and data from another table:
+ * > CREATE TABLE table_name SELECT * FROM database.table_name;
+ * 
+ * - to change table's options:
+ * > ALTER TABLE emp_reports_min CHARACTER SET latin1 ENGINE MyISAM;
+ * 
  * - to describe the table structure:
  * > DESCRIBE employees;
  * 
  * - to know how was the table created:
  * > SHOW CREATE TABLE employees;
+ * 
+ * - rename table:
+ * > RENAME TABLE table_name old_table_name TO new_table_name;
+ * 
+ * - empty table from data - in some cases you won't be able to do this with a foreign key constraints:
+ * > TRUNCATE TABLE table_name;
+ * 
+ * - to delete table:
+ * > DROP TABLE table_name;
+ * 
+ * COLUMNS:
+ * 
+ * - Adding a new column, using FIRST or AFTER to position the column or left blank to add it in the end:
+ * > ALTER TABLE employees ADD COLUMN phone VARCHAR(12) AFTER address;
+ * 
+ * - rename column:
+ * > ALTER TABLE table_name RENAME COLUMN old_column_name TO new_column_name;
+ * 
+ * - modify existing column's data type or attributes:
+ * > ALTER TABLE employees MODIFY position VARCHAR(15) NOT NULL;
+ * 
+ * - change existing column by deleting it and add a new one instead:
+ * > ALTER TABLE employees CHANGE COLUMN code sec_code CHAR(30) NOT NULL;
+ * 
+ * - deleting column:
+ * > ALTER TABLE employees DROP COLUMN dummy;
  * 
  * 
  */
